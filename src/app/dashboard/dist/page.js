f15c83a0a70c2afa5023d0e37601399910150d30
Reid Quisenberry <reid.quisenberry@insightsoftware.com>
2025-01-02 14:59:11 -0500

    first commit

---
+++ src/app/dashboard/dist/page.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var Dashboard = function () {
+    return (React.createElement("div", null,
+        React.createElement("h1", null, "Dashboard")));
+};
+exports["default"] = Dashboard;
---
+++ src/app/dist/page.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var page_1 = require("@/app/dashboard/page");
+function Home() {
+    return (React.createElement(page_1["default"], null));
+}
+exports["default"] = Home;
